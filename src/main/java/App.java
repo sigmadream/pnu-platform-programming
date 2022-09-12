1b910eef8be5562690e75e871564e4cd2a3dc4a3
Sangkon Han <sigmadream@gmail.com>
2022-09-13 02:53:58 +0900

    2주차 수업
    - TODO의 시작

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("===============================================");
+        System.out.println("<<TODOList for CLI>>");
+        System.out.println("- 'help'를 입력하시면 사용법을 확인하실 수 있습니다.");
+        System.out.println("===============================================");
+
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        System.out.println("Your Input is " + input);
+
+        while (true) {
+            System.out.printf(">> ");
+            String command = sc.next();
+            switch (command) {
+                case "help":
+                    System.out.println("Help");
+                    break;
+                default:
+                    System.out.println("Help?");
+            }
+        }
     }
 }
